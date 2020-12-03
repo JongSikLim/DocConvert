c42c03c2d55f6691c74ce0d1bdd31d29d15d11f4
jongsiklim <whdtlr1218@gmail.com>
2020-12-03 17:13:08 +0900

    [feature] 라이브러리 변경 file-convert

--- src/service/pptManager.js
+++ src/service/pptManager.js
@@ -1,22 +1,30 @@
-import Manager from 'ppt-png';
+import document from 'file-convert';
 import fs from 'fs';
 import path from 'path';
-const sample = fs.readFileSync(path.join(__dirname, '../assets/sample.pptx'));
+
+const filePath = path.join(__dirname, '../assets/sample.pptx');
+const sample = fs.readFileSync(filePath);
+
+const options = {
+  libreofficeBin: '/usr/bin/soffice',
+  sourceFile: filePath, // .ppt, .pptx, .odp, .key and .pdf
+  outputDir: '../output',
+  img: true,
+  imgExt: 'png', // Optional and default value png
+  reSize: 800, //  Optional and default Resize is 1200
+  density: 120, //  Optional and default density value is 120
+};
 
 export default {
   convert: (file, next) => {
-    console.log('call Convert');
-    new Manager({
-      files: [...sample],
-      output: 'output/',
-      invert: true,
-      outputType: 'png',
-      logLevel: 2,
-      callback: function (data) {
-        console.log('response');
-        console.log('data:', data);
+    document
+      .convert(options)
+      .then((res) => {
+        console.log('res: ', res);
         next();
-      },
-    }).run();
+      })
+      .catch((err) => {
+        console.log('err: ', err);
+      });
   },
 };
