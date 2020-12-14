bc736092e447f0e9ee1507e8647b603d9a55875c
jongsiklim <whdtlr1218@gmail.com>
2020-12-14 12:18:18 +0900

    [feature] 우분투 테스트용 업데이트

--- src/service/pptManager.js
+++ src/service/pptManager.js
@@ -1,4 +1,18 @@
 import fs from 'fs';
 import path from 'path';
+import Converter from 'ppt-png';
 
-export default {};
+const filePath = path.join(__dirname, '../assets/sample.ppt');
+
+export default {
+  convert: () => {
+    new Converter({
+      files: [...filePathq],
+      output: 'output/',
+      logLevel: 2,
+      callback: function (data) {
+        console.log(data.failed, data.success, data.files, data.time);
+      },
+    }).run();
+  },
+};
