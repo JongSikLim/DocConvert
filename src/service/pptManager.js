15004d5abaaddb9772863a17a2823f4cb4797f62
jongsiklim <whdtlr1218@gmail.com>
2020-12-14 14:48:30 +0000

    success ppt-png

--- src/service/pptManager.js
+++ src/service/pptManager.js
@@ -3,28 +3,23 @@ import path from 'path';
 import Converter from 'ppt-png';
 import unoconv from 'unoconv';
 
-const filePath = path.join(__dirname, '../assets/sample.ppt');
+const filePath = path.join(__dirname, '../assets/sample.pptx');
 const outputPath = path.join(__dirname, '../output/');
+
+console.log('hey', filePath);
 
 export default {
   convert: () => {
     new Converter({
-      files: [...filePath],
+	    files: [filePath],
       output: outputPath,
-      logLevel: 2,
+      invert: outputPath,
+	    logLevel: 2,
       deletePdfFile: true,
-      callback: function (data) {
+      documentConvert: 'libreoffice --headless --invisible --convert-to pdf *.pptx --outdir',
+	    callback: function (data) {
         console.log(data.failed, data.success, data.files, data.time);
       },
     }).run();
   },
-  unoconvConvert: ()=>{
-	  unoconv.convert(
-		  filePath, 
-		  'pdf', 
-		  (err, result)=>{
-			  fs.writeFile('test.pdf',result);
-		  }
-	  );
-  }
 };
