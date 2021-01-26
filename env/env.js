5d7323e8606b9ad588af63ab89e708bb77ae0590
jongsiklim <whdtlr1218@gmail.com>
2021-01-26 19:37:23 +0900

    [feature]
    env prod 버전 키워드 변경

--- env/env.js
+++ env/env.js
@@ -2,8 +2,8 @@ import path from 'path';
 import dotenv from 'dotenv';
 
 // 단일 엔트리 지점으로 환경변수 설정
-if (process.env.NODE_ENV === 'production') {
-  dotenv.config({ path: path.join(__dirname, '.env.production') });
+if (process.env.NODE_ENV === 'prod') {
+  dotenv.config({ path: path.join(__dirname, '.env.prod') });
 } else if (process.env.NODE_ENV === 'development') {
   dotenv.config({ path: path.join(__dirname, '.env.development') });
 } else {
